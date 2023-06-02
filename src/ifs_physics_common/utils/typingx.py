0945a817bb5ba32a8ef830ef6b63787cde3d1c0e
stubbiali <subbiali@phys.ethz.ch>
2023-06-03 00:12:24 +0200

    Add PropertyDict.

--- src/ifs_physics_common/utils/typingx.py
+++ src/ifs_physics_common/utils/typingx.py
@@ -3,6 +3,7 @@ import numpy as np
 from typing import Dict, TypeVar, Union
 
 from sympl import DataArray as SymplDataArray
+from sympl._core.typingx import PropertyDict as SymplPropertyDict
 
 try:
     import cupy as cp
@@ -13,6 +14,7 @@ except ImportError:
 DataArray = SymplDataArray
 DataArrayDict = Dict[str, DataArray]
 ParameterDict = Dict[str, Union[bool, float, int]]
+PropertyDict = SymplPropertyDict
 Storage = Union[np.ndarray, cp.ndarray]
 StorageDict = Dict[str, Storage]
 Range = TypeVar("Range")
