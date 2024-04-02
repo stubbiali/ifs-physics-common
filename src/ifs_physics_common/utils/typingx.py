152a583c022b0e026f89d59f19cb8b224095a201
stubbiali <subbiali@phys.ethz.ch>
2024-04-02 11:26:22 +0200

    Add license.

--- src/ifs_physics_common/utils/typingx.py
+++ src/ifs_physics_common/utils/typingx.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2022-2024 ETH Zurich
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from collections.abc import Hashable, Sequence
 import numpy as np
 import numpy.typing as npt
@@ -14,8 +29,8 @@ except ImportError:
 
 DataArray: TypeAlias = SymplDataArray
 DataArrayDict: TypeAlias = SymplDataArrayDict
-NDArrayLike = Union[npt.NDArray, cp.ndarray]
-NDArrayLikeDict = Dict[str, NDArrayLike]
+NDArray = Union[npt.NDArray, cp.ndarray]
+NDArrayDict = Dict[str, NDArray]
 ParameterDict = Dict[str, Union[bool, float, int]]
 Property = Dict[str, Union[str, Sequence[str], Hashable]]
 PropertyDict = Dict[str, Property]
