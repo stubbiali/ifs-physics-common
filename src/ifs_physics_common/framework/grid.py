f9c9676972516f7ed7024ee9968d21c40f2df398
stubbiali <subbiali@phys.ethz.ch>
2024-05-21 00:21:47 +0200

    Refactor dims and grids.

--- src/ifs_physics_common/framework/grid.py
+++ src/ifs_physics_common/framework/grid.py
@@ -15,6 +15,7 @@
 # limitations under the License.
 
 from __future__ import annotations
+from collections import namedtuple
 from functools import cached_property, lru_cache
 import numpy as np
 from typing import Union, TYPE_CHECKING
@@ -22,44 +23,57 @@ from typing import Union, TYPE_CHECKING
 if TYPE_CHECKING:
     from collections.abc import Hashable
     from numpy.typing import NDArray
+    from typing import Literal, Optional
 
     from ifs_physics_common.framework.config import DomainConfig
 
 
-class AbstractDim:
-    """Symbol identifying a dimension, e.g. I or I-1/2."""
+DIM_INSTANCES = {}
 
-    _instances: dict[int, AbstractDim] = {}
 
+class MetaDim(type):
+    def __call__(cls, *args):
+        key = hash(args)
+        if key not in DIM_INSTANCES:
+            obj = super().__call__(*args)
+            DIM_INSTANCES[key] = obj
+        return DIM_INSTANCES[key]
+
+
+class AbstractGridDim(metaclass=MetaDim):
     name: str
     offset: float
+    direction: Literal[-1, 1]
 
-    def __new__(cls, *args: Hashable) -> AbstractDim:
-        key = hash(args)
-        if key not in cls._instances:
-            cls._instances[key] = super().__new__(cls)
-        return cls._instances[key]
-
-    def __init__(self, name: str, offset: float = 0) -> None:
+    def __init__(self, name: str, offset: float = 0, direction: Literal[-1, 1] = 1) -> None:
         self.name = name
+        assert offset in (-0.5, 0, 0.5)
         self.offset = offset
+        self.direction = direction
 
-    def __add__(self, other: float) -> AbstractDim:
-        return AbstractDim(self.name, self.offset + other)
+    def __add__(self, other: float) -> AbstractGridDim:
+        return AbstractGridDim(self.name, self.offset + other)
 
-    def __sub__(self, other: float) -> AbstractDim:
+    def __sub__(self, other: float) -> AbstractGridDim:
         return self + (-other)
 
-    def __eq__(self, other: Union[AbstractDim, ConcreteDim]) -> bool:
-        if isinstance(other, ConcreteDim):
+    def __neg__(self) -> AbstractGridDim:
+        return AbstractGridDim(self.name, self.offset, -self.direction)
+
+    def __eq__(self, other: Union[AbstractGridDim, ConcreteGridDim]) -> bool:
+        if isinstance(other, ConcreteGridDim):
             return self == other.abstract_dim
-        elif isinstance(other, AbstractDim):
-            return self.name == other.name and self.offset == other.offset
+        elif isinstance(other, AbstractGridDim):
+            return (
+                self.name == other.name
+                and self.offset == other.offset
+                and self.direction == other.direction
+            )
         else:
             return False
 
     def __hash__(self) -> int:
-        return hash((self.name, self.offset))
+        return hash((self.name, self.offset, self.direction))
 
     def __repr__(self) -> str:
         if self.offset > 0:
@@ -69,27 +83,30 @@ class AbstractDim:
         else:
             return f"{self.name}"
 
-    def concretize(self, domain_config: DomainConfig) -> ConcreteDim:
-        if self.name == "I":
-            return ConcreteDim(self, (domain_config.xmin, domain_config.xmax), domain_config.nx)
+    def concretize(self, domain_config: DomainConfig) -> ConcreteGridDim:
+        if self.name in ("I", "IJ"):
+            return ConcreteGridDim(self, (domain_config.xmin, domain_config.xmax), domain_config.nx)
         elif self.name == "J":
-            return ConcreteDim(self, (domain_config.ymin, domain_config.ymax), domain_config.ny)
+            return ConcreteGridDim(self, (domain_config.ymin, domain_config.ymax), domain_config.ny)
         elif self.name == "K":
-            return ConcreteDim(self, (domain_config.zmin, domain_config.zmax), domain_config.nz)
+            return ConcreteGridDim(self, (domain_config.zmin, domain_config.zmax), domain_config.nz)
+        else:
+            raise ValueError(f"Unknown dimension {repr(self.name)}.")
 
 
-I = AbstractDim("I")
-J = AbstractDim("J")
-K = AbstractDim("K")
+I = AbstractGridDim("I")
+IJ = AbstractGridDim("IJ")
+J = AbstractGridDim("J")
+K = AbstractGridDim("K")
 
 
-class ConcreteDim:
-    abstract_dim: AbstractDim
+class ConcreteGridDim:
+    abstract_dim: AbstractGridDim
     bounds: tuple[float, float]
     size: int
 
     def __init__(
-        self, abstract_dim: AbstractDim, bounds: tuple[float, float], domain_size: int
+        self, abstract_dim: AbstractGridDim, bounds: tuple[float, float], domain_size: int
     ) -> None:
         self.abstract_dim = abstract_dim
         self.bounds = bounds
@@ -97,7 +114,7 @@ class ConcreteDim:
 
     @cached_property
     def padding(self) -> int:
-        return 1 if self.abstract_dim.offset in (-0.5, 0.5) else 0
+        return self.abstract_dim.direction if self.abstract_dim.offset == 0 else 0
 
     @cached_property
     def spacing(self) -> float:
@@ -107,39 +124,92 @@ class ConcreteDim:
     @cached_property
     def coords(self) -> NDArray:
         step = self.spacing if self.bounds[1] > self.bounds[0] else -self.spacing
-        if self.abstract_dim.offset == 0:
-            coords = [self.bounds[0] + 1.5 * i * step for i in range(self.size)]
-        else:
-            coords = [self.bounds[0] + i * step for i in range(self.size + 1)]
+        step_factor = 1.5 if self.abstract_dim.offset == 0 else 1.0
+        coords = [
+            self.bounds[0] + i * step_factor * step for i in range(self.size + abs(self.padding))
+        ]
+        if self.abstract_dim.direction < 0:
+            coords = coords[::-1]
         return np.array(coords)
 
-    def __eq__(self, other: Union[AbstractDim, ConcreteDim]) -> bool:
-        if isinstance(other, AbstractDim):
+    def __eq__(self, other: Union[AbstractGridDim, ConcreteGridDim]) -> bool:
+        if isinstance(other, AbstractGridDim):
             return self.abstract_dim == other
-        elif isinstance(other, ConcreteDim):
+        elif isinstance(other, ConcreteGridDim):
             return self.abstract_dim == other.abstract_dim
         else:
             return False
 
 
-DimTuple = tuple[Union[AbstractDim, ConcreteDim], ...]
+ExpandedDim = namedtuple("ExpandedDim", field_names=[])
+
+
+class DataDim(metaclass=MetaDim):
+    name: str
+    size: int
+    direction: Literal[-1, 1]
+    index: Optional[int]
+
+    def __init__(
+        self, name: str, size: int, direction: Literal[-1, 1] = 1, index: Optional[int] = None
+    ) -> None:
+        self.name = name
+        assert size > 0
+        self.size = size
+        self.direction = direction
+        self.index = index
+
+    def __neg__(self) -> DataDim:
+        return DataDim(self.name, self.size, -self.direction, self.index)
+
+    def __eq__(self, other: DataDim) -> bool:
+        if isinstance(other, DataDim):
+            return (
+                self.name == other.name
+                and self.size == other.size
+                and self.direction == other.direction
+            )
+        else:
+            return False
+
+    def __hash__(self) -> int:
+        return hash((self.name, self.size, self.direction))
+
+    def __getitem__(self, index: int) -> DataDim:
+        assert self.index is None
+        return DataDim(self.name, self.size, self.direction, index)
+
+    def __repr__(self) -> str:
+        return f"{self.name}{self.size}"
+
+    @cached_property
+    def coords(self) -> NDArray:
+        return np.arange(start=0, stop=self.size, step=self.direction)
+
+
+D5 = DataDim("D", 5)
+
+
+AbstractGridDimTuple = tuple[AbstractGridDim, ...]
+DataDimTuple = tuple[DataDim, ...]
+DimTuple = tuple[Union[AbstractGridDim, DataDim, type[ExpandedDim]], ...]
 
 
 class Grid:
     """Grid of points."""
 
-    abstract_dims: DimTuple
-    dims: tuple[str, ...]
+    abstract_dims: AbstractGridDimTuple
+    dim_names: tuple[str, ...]
     ndim: int
     padding: tuple[int, ...]
     shape: tuple[int, ...]
     spacing: tuple[float, ...]
     storage_shape: tuple[int, ...]
 
-    def __init__(self, abstract_dims: DimTuple, domain_config: DomainConfig) -> None:
+    def __init__(self, abstract_dims: AbstractGridDimTuple, domain_config: DomainConfig) -> None:
         self.abstract_dims = abstract_dims
         self.ndim = len(abstract_dims)
-        self.dims = tuple(str(dim) for dim in abstract_dims)
+        self.dim_names = tuple(str(dim) for dim in abstract_dims)
 
         concrete_dims = [dim.concretize(domain_config) for dim in abstract_dims]
         self.shape = tuple(dim.size for dim in concrete_dims)
@@ -164,7 +234,7 @@ class Grid:
         out = f"{self.ndim}-D grid with dimensions: \n"
         for i in range(self.ndim):
             out += (
-                f"* {self.dims[i]}: size={self.shape[i]} spacing={self.spacing[i]} "
+                f"* {self.dim_names[i]}: size={self.shape[i]} spacing={self.spacing[i]} "
                 f"padding={self.padding[i]}"
             )
             if i < self.ndim - 1:
@@ -175,9 +245,17 @@ class Grid:
 class ComputationalGrid:
     """A three-dimensional computational grid consisting of mass and staggered grid points."""
 
-    GRID_LOCATIONS: tuple[DimTuple, ...] = ((I, J, K), (I, J, K - 1 / 2), (I, J), (K,))
+    GRID_LOCATIONS: tuple[AbstractGridDimTuple, ...] = (
+        (I, J, K),
+        (I, J, K - 1 / 2),
+        (I, J),
+        (K,),
+        (K - 1 / 2,),
+    )
 
+    domain_config: DomainConfig
     grids: dict[Hashable, Grid]
 
     def __init__(self, domain_config: DomainConfig) -> None:
+        self.domain_config = domain_config
         self.grids = {dims: Grid(dims, domain_config) for dims in self.GRID_LOCATIONS}
